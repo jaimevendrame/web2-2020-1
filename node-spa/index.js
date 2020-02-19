bd84139d444c33b3ea2a8926b2e2fb47601debd7
Jaime Vendrame <jaimevendrame@MacBook-Pro-de-Jaime.local>
2020-02-19 19:45:15 -0300

    2020-19-02

--- node-spa/index.js
+++ node-spa/index.js
@@ -1,11 +1,11 @@
-var restify = require('restify');
+const restify = require('restify');
 
 const server = restify.createServer({
   name: 'myapp',
   version: '1.0.0'
 });
 
-var knex = require('knex')({
+const knex = require('knex')({
   client: 'mysql',
   connection: {
     host : '127.0.0.1',
@@ -19,11 +19,30 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-server.get('/echo/:name', function (req, res, next) {
-  res.send(req.params);
-  return next();
-});
+
 
 server.listen(8081, function () {
   console.log('%s listening at %s', server.name, server.url);
+});
+
+
+
+server.get('/', function (req, res, next) {
+
+  knex('rest').then((dados)=>{
+    res.send(dados)
+  },next)
+
+  return next();
+});
+
+server.post('/create', function (req, res, next) {
+
+  knex('rest')
+    .insert(req.body)
+    .then((dados)=>{
+    res.send(dados)
+  },next)
+
+  return next();
 });
